0729456b5bd50cfea275a3146b15b86c76756e74
fabogit <fabo.dev.email@gmail.com>
2022-07-15 09:45:17 +0200

    basic start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TicketsComponent } from './components/tickets/tickets.component';
+import { TicketItemComponent } from './components/ticket-item/ticket-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TicketsComponent,
+    TicketItemComponent
   ],
   imports: [
     BrowserModule,
