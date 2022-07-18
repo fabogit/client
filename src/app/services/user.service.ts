3849c7f3871211fe25fc869e4f1fe1af9cd3c420
fabogit <fabo.dev.email@gmail.com>
2022-07-18 08:35:06 +0200

    user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class UserService {
+
+	constructor() { }
+
+	// TODO
+	registerUser() {
+
+	}
+
+	loginUser() {
+
+	}
+
+	logoutUser() {
+
+	}
+
+}
